abacaeee15488c97cfb81652534091cdf0ca0fcf
Will Wybrow <will@willwybrow.com>
2022-01-29 11:11:21 +0000

    Tidy up unused imports

--- src/main/java/uk/wycor/starlines/persistence/neo4j/entity/StarEntity.java
+++ src/main/java/uk/wycor/starlines/persistence/neo4j/entity/StarEntity.java
@@ -6,15 +6,11 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.neo4j.ogm.annotation.CompositeIndex;
-import org.neo4j.ogm.annotation.GeneratedValue;
-import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
-import org.neo4j.ogm.id.UuidStrategy;
 import org.neo4j.ogm.types.spatial.CartesianPoint2d;
 
 import java.util.Set;
-import java.util.UUID;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
